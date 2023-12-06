139b74ae79c5c5bce7bbf3f36310a99d0ef0a937
jeongyusu <onlys2545@naver.com>
2023-12-06 17:53:35 +0900

    임시 엔티티 USER추가

---
+++ src/main/java/com/tenco/indiepicter/user/User.java
@@ -0,0 +1,16 @@
+package com.tenco.indiepicter.user;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+public class User {
+    Integer id;
+    String userEmail;
+    String password;
+    String username;
+    String pic;
+    int grade;
+    String tel;
+    String role;
+    Timestamp created_at;
+}
