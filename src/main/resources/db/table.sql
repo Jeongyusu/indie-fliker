9b9388cd1efdf3c828715276a1b7f177170ef3ba
Jys <onlys2545@naver.com>
2023-12-07 15:48:38 +0900

    더미 작성중

--- src/main/resources/db/table.sql
+++ src/main/resources/db/table.sql
@@ -104,4 +104,96 @@ create table review_tb(
                           foreign KEY(movie_id) references movie_tb(id)
 );
 
+create table vip_review_tb(
+                              id int auto_increment primary key,
+                              content varchar(100) not null,
+                              created_at datetime not null,
+                              point int not null,
+                              user_id int,
+                              movie_id int,
+                              foreign KEY(user_id) references user_tb(id),
+                              foreign KEY(movie_id) references movie_tb(id)
+);
 
+create table running_schedule_tb(
+                                    id int auto_increment primary key,
+                                    running_time int not null,
+                                    running_date datetime not null,
+                                    start_time time not null,
+                                    end_time time not null,
+                                    movie_id int,
+                                    theater_id int,
+                                    foreign KEY(movie_id) references movie_tb(id),
+                                    foreign KEY(theater_id) references theater_tb(id)
+);
+
+create table seat_tb(
+                        id int auto_increment primary key,
+                        seat_name varchar(5) not null,
+                        running_schedule_id int not null,
+                        foreign KEY(running_schedule_id) references running_schedule_tb(id)
+);
+
+
+create table reservation_tb(
+                               id int auto_increment primary key,
+                               reservation_code varchar(50) not null,
+                               reservated_at datetime not null,
+                               user_id int not null,
+                               seat_id int not null,
+                               foreign KEY(user_id) references user_tb(id),
+                               foreign KEY(seat_id) references seat_tb(id)
+);
+
+create table order_tb(
+                         id int auto_increment primary key,
+                         selected_seats varchar(200) not null,
+                         product_price int not null,
+                         quantity int not null,
+                         funding_id int not null,
+                         user_id int not null,
+                         foreign KEY(funding_id) references funding_tb(id),
+                         foreign KEY(user_id) references user_tb(id)
+);
+
+create table payment_tb(
+                           id int auto_increment primary key,
+                           total_price int not null,
+                           discount_price int not null,
+                           final_price int not null,
+                           paymented_at datetime not null,
+                           payment_type_id int not null,
+                           order_id int not null,
+                           foreign KEY(payment_type_id) references payment_tb(id),
+                           foreign KEY(order_id) references order_tb(id)
+);
+
+create table refund_tb(
+                          id int auto_increment primary key,
+                          refund_payment int not null,
+                          refunded_at date not null,
+                          payment_id int not null,
+                          foreign KEY(payment_id) references payment_tb(id)
+);
+
+create table payment_type_tb(
+                                id int auto_increment primary key,
+                                type varchar(20) not null
+);
+
+create table scrab_tb(
+                         id int auto_increment primary key,
+                         user_id int not null,
+                         funding_id int not null,
+                         foreign KEY(user_id) references user_tb(id),
+                         foreign KEY(funding_id) references funding_tb(id)
+);
+
+
+create table notice_tb(
+                          id int auto_increment primary key,
+                          notice_title varchar(20) not null,
+                          content varchar(200) not null,
+                          category varchar(20) not null,
+                          created_at date not null
+);
