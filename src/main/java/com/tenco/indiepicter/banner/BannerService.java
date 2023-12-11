c3dc2f065f246d8f0460e0374eb6bc969295c152
Jys <onlys2545@naver.com>
2023-12-11 12:20:54 +0900

    에러 수정중

--- src/main/java/com/tenco/indiepicter/banner/BannerService.java
+++ src/main/java/com/tenco/indiepicter/banner/BannerService.java
@@ -1,6 +1,7 @@
 package com.tenco.indiepicter.banner;
 
 import com.tenco.indiepicter.funding.FundingResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
