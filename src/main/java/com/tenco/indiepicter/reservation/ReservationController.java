23cd43202a12cc0b66a60e3b7b5b63b234f420c4
namgooo <namgo9204@naver.com>
2023-12-11 14:20:33 +0900

    111

--- src/main/java/com/tenco/indiepicter/reservation/ReservationController.java
+++ src/main/java/com/tenco/indiepicter/reservation/ReservationController.java
@@ -4,22 +4,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/reservation")
 public class ReservationController {
-
+	
 	@Autowired
-	private ReservationService reservationService;
+	public ReservationService reservationService;
 	
-	
-	@GetMapping("/reservationTicket")
+	// localhost/reservation/reservationTicket
+	@GetMapping("/re")
 	public String reservationTicket() {
-		
-		return "";
+		return "reservation/reservation_ticket";
 	}
 	
 	
-	
+	// jsp 파일 테스트 중
 }
