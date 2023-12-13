d7ec9c9bae7c9a844768e688239bda6204f6bfa2
Jys <onlys2545@naver.com>
2023-12-13 11:46:05 +0900

    [정유수] 영화관 DB 필드 추가

--- src/main/java/com/tenco/indiepicter/funding/FundingRepository.java
+++ src/main/java/com/tenco/indiepicter/funding/FundingRepository.java
@@ -3,6 +3,7 @@ package com.tenco.indiepicter.funding;
 
 import com.tenco.indiepicter.funding.response.FindByFundingIdDTO;
 import com.tenco.indiepicter.funding.response.MoviesByGenreDTO;
+import com.tenco.indiepicter.funding.response.OnAirMovieDTO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
@@ -17,4 +18,9 @@ public interface FundingRepository {
     // movie_id로 funding_id 조회
     public FindByFundingIdDTO findByMovieId(Integer movieId);
 
+    //상영중 영화 조회
+    public List<OnAirMovieDTO> findAllByOnAir();
+
+    //상영중 영화 장르별 조회
+    public List<OnAirMovieDTO> findAllByOnAirAndGenre();
 }
