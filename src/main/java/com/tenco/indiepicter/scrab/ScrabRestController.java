deb99f296dc23db5b39e8d8ddf7d585e18e5e50f
KinHaYu <z2poter@naver.com>
2023-12-14 11:10:14 +0900

    [김하율]깃 merge용 커밋

--- src/main/java/com/tenco/indiepicter/scrab/ScrabRestController.java
+++ src/main/java/com/tenco/indiepicter/scrab/ScrabRestController.java
@@ -1,6 +1,7 @@
 package com.tenco.indiepicter.scrab;
 
 import com.tenco.indiepicter._core.utils.ApiUtils;
+import com.tenco.indiepicter.scrab.response.ToggleRequestDTO;
 import com.tenco.indiepicter.scrab.response.ToggleResponseDTO;
 import com.tenco.indiepicter.user.User;
 import lombok.extern.slf4j.Slf4j;
@@ -9,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -18,10 +20,13 @@ public class ScrabRestController {
     private ScrabService scrabService;
 
     @GetMapping("/api/scrabs/view")
-    public ResponseEntity<?> scrabview(HttpServletRequest request){
+    public ResponseEntity<?> scrabview(@RequestParam Integer userId,
+                                       @RequestParam(required = false) Integer fundingId) {
 
+        ToggleRequestDTO toggleRequestDTO = new ToggleRequestDTO(userId, fundingId);
+        List<Scrab> scrabs = scrabService.scrabview(toggleRequestDTO);
 
-        return null;
+        return ResponseEntity.ok().body(ApiUtils.success(scrabs));
     }
 
     @PostMapping("/api/scrabs/toggle")
