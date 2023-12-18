47b0d70499641f5f91aa2f8c7207e4cb2bc2f52f
KinHaYu <z2poter@naver.com>
2023-12-18 18:43:37 +0900

    [김하율] 좋아요 기능 완료

--- src/main/java/com/tenco/indiepicter/_core/utils/BalanceUtil.java
+++ src/main/java/com/tenco/indiepicter/_core/utils/BalanceUtil.java
@@ -4,10 +4,32 @@ import java.text.DecimalFormat;
 
 public class BalanceUtil {
 
+
     // 천 단위 콤마 찍기
     public static String formatBalance(Integer balance){
         DecimalFormat df = new DecimalFormat("###,###");
         String formatMoney = df.format(balance);
         return formatMoney;
     }
+
+    // 달성률 계산 및 정수 값 포맷
+    public static String calculateAndSetAchievementRate(Integer targetPrice, Integer presentPrice) {
+
+        Double achievementRate = 0.0;
+
+        if (targetPrice == 0) {
+            achievementRate = 0.0;
+        } else {
+            // 소숫점 자리를 유지하고 반올림
+            achievementRate = (double) Math.round(((double) presentPrice / targetPrice * 100) * 10) / 10.0;
+        }
+
+        // 출력 시 소수점 이하 자릿수를 표시하지 않도록 설정
+        DecimalFormat decimalFormat = new DecimalFormat("#,##0");
+        String formattedAchievementRate = decimalFormat.format(achievementRate);
+
+        return formattedAchievementRate;
+    }
+
+
 }
--- src/main/java/com/tenco/indiepicter/scrab/response/ScrabResponseDTO.java
+++ src/main/java/com/tenco/indiepicter/scrab/response/ScrabResponseDTO.java
@@ -1,13 +1,14 @@
 package com.tenco.indiepicter.scrab.response;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.tenco.indiepicter._core.utils.BalanceUtil;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.text.DecimalFormat;
+
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class ScrabResponseDTO {
     private Integer fundingId; // 펀딩아이디
     private String thumbnail;  // 썸네일
@@ -20,34 +21,12 @@ public class ScrabResponseDTO {
     @JsonIgnore
     private Integer presentPrice; // 현재금액
 
-    private Double achievementRate; // 달성률
-
     private String movieName; // 영화제목
     private String synopsis; // 시놉시스
     private String production; // 배급사
 
-    public ScrabResponseDTO(Integer fundingId, String thumbnail, Integer targetPrice, Integer presentPrice, String movieName, String synopsis, String production) {
-        this.fundingId = fundingId;
-        this.thumbnail = thumbnail;
-        this.targetPrice = targetPrice;
-        this.presentPrice = presentPrice;
-        this.movieName = movieName;
-        this.synopsis = synopsis;
-        this.production = production;
-        calculateAndSetAchievementRate();
-    }
-
-    public void calculateAndSetAchievementRate() {
-        System.out.println("targetPrice: " + targetPrice);
-        System.out.println("presentPrice: " + presentPrice);
-
-        if (targetPrice == 0) {
-            this.achievementRate = 0.0;
-        } else {
-            double rawAchievementRate = (double) presentPrice / targetPrice * 100;
-            this.achievementRate = Math.round(rawAchievementRate * 10.0) / 10.0; // 반올림하여 소수점 아래 한 자리까지 표시
-        }
-
-        System.out.println("calculated achievementRate: " + achievementRate);
+    // 달성률 계산 및 정수 값 포맷
+    public String calculateAndSetAchievementRate(){
+        return BalanceUtil.calculateAndSetAchievementRate(targetPrice, presentPrice);
     }
 }
