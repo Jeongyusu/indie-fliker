8fd986bf981895e2288dd8d2297df0c340ebc4d7
muaga <more9494@naver.com>
2023-12-21 13:58:39 +0900

    move

--- src/main/java/com/tenco/indiepicter/chat/ChatController.java
+++ src/main/java/com/tenco/indiepicter/chat/ChatController.java
@@ -1,19 +1,27 @@
 package com.tenco.indiepicter.chat;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
+@RequestMapping("/chat")
 public class ChatController {
-	
-    @PostMapping("/chatroom")
-    public String enterChatroom(@RequestBody String movieTitle) {
+
+    @Autowired
+    private ChatService chatService;
+
+
+    @PostMapping("/open-movie")
+    public String openMovieChatRoom() {
     	
     	// 인증검사 필요!!! (로그인 인증 & vip인증?)
     	
-    	// 영화정보 필요합니다! (model로 던져서 chatroom.jsp도 바꿔줘야함)
-
+    	// 영화정보 필요합니다! (model로 던져서 chatroom.jsp도 바꿔줘야함
         return "main/chatroom";
     }
     
